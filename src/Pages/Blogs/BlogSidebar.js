2375d9870e7980830554b0588c522a9ca1dcdeb6
akazad1235 <akazad914@gmail.com>
2020-11-21 02:33:10 +0600

    blog category section added

--- src/Pages/Blogs/BlogSidebar.js
+++ src/Pages/Blogs/BlogSidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const BlogSidebar = () => {
@@ -11,6 +12,19 @@ const BlogSidebar = () => {
                <button className="btn btn-danger"><i class="fas fa-search"></i></button>
             </div>
            </form>
+           <div>
+               <h2 className="my-4" style={{borderLeft:'4px solid', borderColor:'#C82333'}}><span className="ml-2 font-weight-bold">Category</span></h2>
+               <div className="cat-list"> 
+                    
+                        <li className="p-2"><a href="">Web Design</a><span class="badge badge-dark float-right">15</span></li>
+                        <li className="p-2"><a href="">Web Design</a><span class="badge badge-dark float-right">15</span></li>
+                        <li className="p-2"><a href="">Web Design</a><span class="badge badge-dark float-right">15</span></li>
+                        <li className="p-2"><a href="">Web Design</a><span class="badge badge-dark float-right">15</span></li>
+   
+               </div>
+           </div>
+
+
            
         </>
     );
