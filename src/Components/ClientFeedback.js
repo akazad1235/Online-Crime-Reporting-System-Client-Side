e9d159f802b7e088d1b0b88dd79c4d8123c7cd94
akazad1235 <akazad914@gmail.com>
2021-01-02 10:19:02 +0600

    feedback show home page

--- src/Components/ClientFeedback.js
+++ src/Components/ClientFeedback.js
@@ -1,12 +1,30 @@
+import Axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useRef } from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import Slider from 'react-slick';
 import client1 from '../Assets/images/customer-1.png';
-
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 const ClientFeedback = () => {
+
+  const [feedback, setFeeddback] = useState([]);
+
     const customSlider = useRef();
+
+    useEffect(()=>{
+      Axios.get('http://localhost:8000/api/feedback')
+      .then(res =>{
+        console.log(res.data);
+        setFeeddback(res.data);
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+    },[])
 
     const previous = () => {
       customSlider.current.slickPrev();
@@ -23,6 +41,7 @@ const ClientFeedback = () => {
           slidesToShow: 2,
           slidesToScroll: 1
         };
+        console.log(feedback);
     return (
         <div id="testimonials">
         <h1 className="text-center my-3">Victim Feedback</h1>
@@ -33,7 +52,7 @@ const ClientFeedback = () => {
         
       <Container>
       <Slider ref={c => (customSlider.current = c)} {  ...settings}>
-            <div className="text-center testimonials-container">
+            {/* <div className="text-center testimonials-container">
               <img  className="rounded-circle mx-auto d-block "  src={client1} alt=""/>
                     <h2>this is azad</h2>
                     <h5 className="text-danger">Desc</h5>
@@ -51,7 +70,21 @@ const ClientFeedback = () => {
                     <h2>this is azad</h2>
                     <h5 className="text-danger">Desc</h5>
                     <p className="px-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quo totam aspernatur cum nobis odio labore consequatur veniam eaque asperiores.</p>
-             </div>
+             </div> */}
+             {
+               feedback.map((feedback) => {
+                return<div className="text-center testimonials-container">
+                <img  className="rounded-circle mx-auto d-block "  src={client1} alt=""/>
+                      <h2>{feedback.name}</h2>
+                      <h5 className="text-danger p-2 flex-grow-1 ">{feedback.profession}</h5>
+                      <p className="px-5">{feedback.desc.substr(0,100)}</p>
+                      <div className="d-flex justify-content-center">
+                      <p className="text-primary mr-5">{moment(feedback.created_at).fromNow()}</p>
+                     <Button className="btn btn-danger btn-sm m-1"><Link to="" className="text-white">More</Link></Button>
+                      </div>
+               </div>
+               })
+             }
            
            
          </Slider>
